60ae7bee751473e0deb639b5326274e0d43f5544
이현서 <slrkanjfkrn@gmail.com>
2025-02-25 16:48:58 +0900

    modify

--- app.py
+++ app.py
@@ -25,9 +25,7 @@ st.markdown(
 """
 )
 
-llm = ChatOpenAI(
-    temperature=0.1,
-)
+
 
 if "openai_api_key" not in st.session_state:
     st.session_state["openai_api_key"] = ""
@@ -77,76 +75,6 @@ choose_prompt = ChatPromptTemplate.from_messages(
         ("human", "{question}"),
     ]
 )
-def get_answers(inputs):
-    docs = inputs["docs"]
-    question = inputs["question"]
-    answers_chain = answers_prompt | llm
-    return {
-        "question": question,
-        "answers": [
-            {
-                "answer": answers_chain.invoke(
-                    {"question": question, "context": doc.page_content}
-                ).content,
-                "source": doc.metadata["source"],
-                "date": doc.metadata["lastmod"],
-            }
-            for doc in docs
-        ],
-    }
-
-
-
-def choose_answer(inputs):
-    answers = inputs["answers"]
-    question = inputs["question"]
-    choose_chain = choose_prompt | llm
-    condensed = "\n\n".join(
-        f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n"
-        for answer in answers
-    )
-    return choose_chain.invoke(
-        {
-            "question": question,
-            "answers": condensed,
-        }
-    )
-
-
-def parse_page(soup):
-    header = soup.find("header")
-    footer = soup.find("footer")
-    if header:
-        header.decompose()
-    if footer:
-        footer.decompose()
-    return (
-        str(soup.get_text())
-        .replace("\n", " ")
-        .replace("\xa0", " ")
-        .replace("CloseSearch Submit Blog", "")
-    )
-
-
-@st.cache_resource(show_spinner="Loading website...")
-def load_website(url):
-    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=1000,
-        chunk_overlap=200,
-    )
-    loader = SitemapLoader(
-        url,
-        filter_urls=[
-            r"^(.*\/workers-ai\/).*",
-            r"^(.*\/vectorize\/).*",
-            r"^(.*\/ai-gateway\/).*",
-        ],
-        parsing_function=parse_page,
-    )
-    loader.requests_per_second = 2
-    docs = loader.load_and_split(text_splitter=splitter)
-    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
-    return vector_store.as_retriever()
 
 with st.sidebar:
     openai_api_key = st.text_input("Enter OpenAI API Key", type="password")
@@ -162,6 +90,76 @@ with st.sidebar:
 api_key = os.getenv("OPENAI_API_KEY", st.session_state.get("openai_api_key", ""))
 
 if api_key :
+    llm = ChatOpenAI(temperature=0.1)
+
+    def get_answers(inputs):
+        docs = inputs["docs"]
+        question = inputs["question"]
+        answers_chain = answers_prompt | llm
+        return {
+            "question": question,
+            "answers": [
+                {
+                    "answer": answers_chain.invoke(
+                        {"question": question, "context": doc.page_content}
+                    ).content,
+                    "source": doc.metadata["source"],
+                    "date": doc.metadata["lastmod"],
+                }
+                for doc in docs
+            ],
+        }
+
+    def choose_answer(inputs):
+        answers = inputs["answers"]
+        question = inputs["question"]
+        choose_chain = choose_prompt | llm
+        condensed = "\n\n".join(
+            f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n"
+            for answer in answers
+        )
+        return choose_chain.invoke(
+            {
+                "question": question,
+                "answers": condensed,
+            }
+        )
+
+    def parse_page(soup):
+        header = soup.find("header")
+        footer = soup.find("footer")
+        if header:
+            header.decompose()
+        if footer:
+            footer.decompose()
+        return (
+            str(soup.get_text())
+            .replace("\n", " ")
+            .replace("\xa0", " ")
+            .replace("CloseSearch Submit Blog", "")
+        )
+
+    @st.cache_resource(show_spinner="Loading website...")
+    def load_website(url):
+        splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=1000,
+            chunk_overlap=200,
+        )
+        loader = SitemapLoader(
+            url,
+            filter_urls=[
+                r"^(.*\/workers-ai\/).*",
+                r"^(.*\/vectorize\/).*",
+                r"^(.*\/ai-gateway\/).*",
+            ],
+            parsing_function=parse_page,
+        )
+        loader.requests_per_second = 2
+        docs = loader.load_and_split(text_splitter=splitter)
+        vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+        return vector_store.as_retriever()
+
+
     url = "https://developers.cloudflare.com/sitemap-0.xml"
     retriever = load_website(url)
     query = st.text_input("Ask a question to the website.")
